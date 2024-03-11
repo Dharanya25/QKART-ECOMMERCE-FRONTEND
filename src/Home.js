bae40b79be0c092e4fdab468f94aebeef53dfa76
Dharanya25 <Dharanya25@gmail.com>
2024-03-11 04:04:45 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://w1.pngwing.com/pngs/259/41/png-transparent-online-shopping-sales-ecommerce-online-and-offline-pakistan-retail-customer-final-good-thumbnail.png" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://backend-ten-alpha-42.vercel.app"
